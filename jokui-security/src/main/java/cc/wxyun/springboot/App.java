d52f96b6b085b7c320b9c5bf08ba3d9da8ecbb6f
wanxing <992917052@qq.com>
2019-05-02 23:24:14 +0800

    初次提交项目

---
+++ jokui-security/src/main/java/cc/wxyun/springboot/App.java
@@ -0,0 +1,14 @@
+package cc.wxyun.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 启动类
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ jokui-security/src/main/java/cc/wxyun/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cc.wxyun.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name){
+        return "hello" + name;
+    }
+}
